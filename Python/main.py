25ddefa29de2388cfc33f104b39a68dd0083cd0f
Raul Madero <r.madero.ramirez@gmail.com>
2023-06-27 11:09:45 -0600

    Objetos

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,8 @@
 from car import Car
+from account import Account
 
 
 if __name__ == "__main__":
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Andres Herrera"
+    car = Car("AMS234", Account("Andres Herrera", "ANDA7894"))
     print(vars(car))
+    print(vars(car.driver))
